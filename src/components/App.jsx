36f2722ed51e13bc03dabe6d633318234c260267
wilsonm8 <103072620@student.swin.edu.au>
2025-02-22 18:07:32 +1100

    fetched data from api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,31 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Card from './Card'
 import Score from './Score'
 import '../styles/App.css'
 
 function App() {
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const names = ['geodude', 'pikachu', 'charizard'];
+        const responses = await Promise.all(
+          names.map(name => fetch(`https://pokeapi.co/api/v2/pokemon/${name}`))
+        );
+        const data = await Promise.all(responses.map(response => response.json()));
+        setData(data); 
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchPokemon();
+  }, []);
+  
   return (
     <>
-      
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
     </>
   )
 }
